7f8e211fe58f0c116dc98f32ab65fe3b6718c273
Alex Grytsai <a.grytsai1987@gmail.com>
2025-06-06 10:21:20 +0300

    feat: Implement graceful shutdown
    This commit introduces a `ShutdownService` that orchestrates a graceful shutdown of the application.
    
    The service performs the following actions:
    
    - Sets the readiness probe to false, preventing new requests.
    - Awaits all pending tasks in the `EventsService` to complete.
    - Calls `onModuleDestroy` on the `NatsPublisher` to gracefully disconnect from NATS.
    - Calls `onModuleDestroy` on the `PrismaService` to gracefully close the database connection.

--- src/services/shutdown.service.ts
+++ src/services/shutdown.service.ts
@@ -1,1 +1,22 @@
-export class ShutdownService {} 
+import { Injectable } from '@nestjs/common'
+import { HealthService } from '../health/health.service'
+import { EventsService } from '../modules/events/event.service'
+import { PrismaService } from './prisma.service'
+import { NatsPublisher } from '../modules/nats/nats.publisher'
+
+@Injectable()
+export class ShutdownService {
+  constructor(
+    private readonly health: HealthService,
+    private readonly events: EventsService,
+    private readonly prisma: PrismaService,
+    private readonly nats: NatsPublisher
+  ) {}
+
+  async shutdown() {
+    this.health.setReadiness(false)
+    await this.events.awaitAllTasksDone()
+    await this.nats.onModuleDestroy()
+    await this.prisma.onModuleDestroy()
+  }
+} 
