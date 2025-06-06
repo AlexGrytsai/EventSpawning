88298eda7ca3d6f503e199cb95d6683700ce598a
Alex Grytsai <a.grytsai1987@gmail.com>
2025-06-06 15:26:00 +0300

    feat: Enhance NATS connection resilience with indefinite reconnection
    This commit improves the NATS connection handling by enabling indefinite reconnection attempts.
    
    Changes include:
    
    - Added detailed JSDoc comments to the NATS connection factory function, explaining its purpose, parameters, and behavior.
    - Configured the NATS client to reconnect indefinitely by setting `maxReconnectAttempts` to -1.
    - Set `reconnectTimeWait` to 2000 milliseconds.

--- src/modules/nats/nats.service.ts
+++ src/modules/nats/nats.service.ts
@@ -9,6 +9,19 @@ import { connect } from 'nats'
     ConfigService,
     {
       provide: 'NATS_JS',
+      /**
+       * A factory function that connects to a NATS server and sets up a JetStream client.
+       * The `NATS_URL` environment variable is used to determine the URL to connect to, and
+       * defaults to `nats://localhost:4222` if not set.
+       *
+       * The `reconnect` option is set to true, and the `maxReconnectAttempts` is set to -1
+       * to indicate that the client should reconnect indefinitely. The `reconnectTimeWait`
+       * is set to 2000 milliseconds.
+       *
+       * The function returns a promise that resolves with a JetStream client.
+       *
+       * @param config - a ConfigService instance, injected by NestJS.
+       */
       useFactory: async (config: ConfigService) => {
         const natsUrl = config.get('NATS_URL') ?? 'nats://localhost:4222'
         const nc = await connect({
