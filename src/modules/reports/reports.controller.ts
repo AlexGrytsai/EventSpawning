324458b60740fb58c4b2e879c699bbcf734a3e65
Alex Grytsai <a.grytsai1987@gmail.com>
2025-06-07 12:58:04 +0300

    feat(reports): add demographics report endpoint
    This commit introduces a new endpoint `/reports/demographics` to retrieve demographics reports. It includes:
    
    - Implementation of the `getDemographicsReport` controller method.
    - Parsing of query parameters using `DemographicsReportFilterSchema`.
    - Passing the parsed filter and correlation ID to the `reportsService`.

--- src/modules/reports/reports.controller.ts
+++ src/modules/reports/reports.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Query } from '@nestjs/common'
 import { ReportsService } from './reports.service'
 import { EventsReportFilterDto } from './events-report-filter.dto'
+import { DemographicsReportFilterSchema } from '../../reporter/dto/demographics-report-filter.dto'
 
 @Controller('reports')
 export class ReportsController {
@@ -10,5 +11,12 @@ export class ReportsController {
   async getEventsReport(@Query() query: any) {
     const filter = EventsReportFilterDto.parse(query)
     return this.reportsService.getEventsReport(filter)
+  }
+
+  @Get('demographics')
+  async getDemographicsReport(@Query() query: any) {
+    const correlationId = query.correlationId || ''
+    const filter = DemographicsReportFilterSchema.parse(query)
+    return this.reportsService.getDemographicsReport(filter, correlationId)
   }
 } 
