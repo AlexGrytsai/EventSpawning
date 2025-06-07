60be124723e37d29f773c7f08e414d7b520dd52a
Alex Grytsai <a.grytsai1987@gmail.com>
2025-06-07 09:53:58 +0300

    feat: Integrate ConfigModule for environment variable management
    This commit introduces the `ConfigModule` to the application, enabling the loading and management of environment variables.
    
    The following changes were made:
    
    - Imported and configured the `ConfigModule` with global scope.
    - Configured `ConfigModule` to load environment variables from `.env` files based on the `SERVICE_ENV` environment variable (e.g., `.env.development`, `.env.production`).  If `SERVICE_ENV` is not set, it defaults to `.env`.
    - Enabled variable expansion and caching for the `ConfigModule`.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { MetricsModule } from './modules/metrics/metrics.module'
 import { HealthModule } from './health/health.module'
 
 @Module({
-    imports: [MetricsModule, HealthModule],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: process.env.SERVICE_ENV ? `.env.${process.env.SERVICE_ENV}` : '.env',
+            expandVariables: true,
+            cache: true,
+        }),
+        MetricsModule,
+        HealthModule,
+    ],
 })
 export class AppModule {}
