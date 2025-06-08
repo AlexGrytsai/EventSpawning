28f4857fba62a73241cdf1d19930934605f7e195
Alex Grytsai <a.grytsai1987@gmail.com>
2025-06-08 16:25:48 +0300

    feat(events): Refactor events module dependencies
    This commit refactors the events module to improve its structure and reduce direct dependencies.
    
    Changes:
    - Removed direct imports of `LoggerService`, `HealthService`, and `CorrelationIdService`.
    - Added import of `LoggerModule`, `HealthModule` and `NatsModule` to manage their services as a module.

--- src/modules/events/events.module.ts
+++ src/modules/events/events.module.ts
@@ -1,23 +1,20 @@
 import { Module } from '@nestjs/common'
 import { EventsController } from './controllers/events.controller'
 import { EventsService } from './services/event.service'
-import { LoggerService } from '../../common/services/logger.service'
-import { HealthService } from '../health/services/health.service'
+import { HealthModule } from '../health/health.module'
 import { PrismaService } from '../../common/services/prisma.service'
 import { NatsPublisher } from '../nats/services/nats.publisher'
-import { CorrelationIdService } from '../../common/services/correlation-id.service'
 import { MetricsModule } from '../metrics/metrics.module'
+import { LoggerModule } from '../../common/services/logger.module'
+import { NatsModule } from '../nats/nats.module'
 
 @Module({
-  imports: [MetricsModule],
+  imports: [MetricsModule, LoggerModule, HealthModule, NatsModule],
   controllers: [EventsController],
   providers: [
     EventsService,
-    LoggerService,
-    HealthService,
     PrismaService,
     NatsPublisher,
-    CorrelationIdService,
   ],
   exports: [EventsService],
 })
