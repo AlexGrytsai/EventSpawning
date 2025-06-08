3451890bd04293164229058e6becce06302b253f
Alex Grytsai <a.grytsai1987@gmail.com>
2025-06-08 11:35:12 +0300

    feat: Decouple MetricsService and ensure Prometheus registry reuse
    This commit refactors the code to decouple the `MetricsService` from the `EventsModule` and `ReportsModule` and ensures reuse of existing metrics in the Prometheus registry.
    
    The following changes were made:
    
    - Removed `MetricsService` from the `EventsModule` and `ReportsModule` providers.
    - Added `MetricsModule` to the `EventsModule` and `ReportsModule` imports to provide access to the prometheus registry
    - Modified `MetricsService` to check for existing metrics in the Prometheus registry before creating new ones, thus ensuring metrics consistency across the application.

--- src/modules/events/events.module.ts
+++ src/modules/events/events.module.ts
@@ -2,18 +2,18 @@ import { Module } from '@nestjs/common'
 import { EventsController } from './controllers/events.controller'
 import { EventsService } from './services/event.service'
 import { LoggerService } from '../../common/services/logger.service'
-import { MetricsService } from '../metrics/services/metrics.service'
 import { HealthService } from '../health/services/health.service'
 import { PrismaService } from '../../common/services/prisma.service'
 import { NatsPublisher } from '../nats/services/nats.publisher'
 import { CorrelationIdService } from '../../common/services/correlation-id.service'
+import { MetricsModule } from '../metrics/metrics.module'
 
 @Module({
+  imports: [MetricsModule],
   controllers: [EventsController],
   providers: [
     EventsService,
     LoggerService,
-    MetricsService,
     HealthService,
     PrismaService,
     NatsPublisher,
--- src/modules/reports/reports.module.ts
+++ src/modules/reports/reports.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common'
 import { ReportsController } from './controllers/reports.controller'
 import { ReportsService } from './services/reports.service'
-import { MetricsService } from '../metrics/services/metrics.service'
+import { MetricsModule } from '../metrics/metrics.module'
 import { LoggerModule } from '../../common/services/logger.module'
 
 @Module({
-  imports: [LoggerModule],
+  imports: [LoggerModule, MetricsModule],
   controllers: [ReportsController],
-  providers: [ReportsService, MetricsService],
+  providers: [ReportsService],
 })
 export class ReportsModule {} 
