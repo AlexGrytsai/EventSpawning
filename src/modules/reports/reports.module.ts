f6d8af89f868f6629c6baf427e793760e1a948a6
Alex Grytsai <a.grytsai1987@gmail.com>
2025-06-08 09:30:36 +0300

    feat(reports): Remove PrismaService from ReportsModule
    This commit removes `PrismaService` from the `ReportsModule` providers. This change is likely due to a refactoring where `PrismaService` is no longer directly needed within the `ReportsModule` itself, potentially being accessed through another service or dependency.

--- src/modules/reports/reports.module.ts
+++ src/modules/reports/reports.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common'
 import { ReportsController } from './controllers/reports.controller'
 import { ReportsService } from './services/reports.service'
-import { PrismaService } from '../../common/services/prisma.service'
-import { LoggerModule } from '../../common/services/logger.module'
 import { MetricsService } from '../metrics/services/metrics.service'
+import { LoggerModule } from '../../common/services/logger.module'
 
 @Module({
   imports: [LoggerModule],
   controllers: [ReportsController],
-  providers: [ReportsService, PrismaService, MetricsService],
+  providers: [ReportsService, MetricsService],
 })
 export class ReportsModule {} 
